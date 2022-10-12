7fe2e45b4ea6426a9fe67705e0f263cda421f22e
Niveditha-01 <113326208+Niveditha-01@users.noreply.github.com>
2022-10-12 17:11:36 +0530

    Add files via upload

---
+++ GlobalExchangeCenterApplication.java
@@ -0,0 +1,13 @@
+package com.gl.globalExchangeCenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobalExchangeCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlobalExchangeCenterApplication.class, args);
+	}
+
+}
